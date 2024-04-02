eda9bb7ab1bb0486bf8cb0e7f6e3094dafe9ea26
parthbhatt268 <parthbhatt268@gmail.com>
2024-04-02 21:24:59 +0100

    Base Application Code

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Cians',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Snuggy',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Returned'
+    },
+    {
+        productName: 'Black & Blue',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
